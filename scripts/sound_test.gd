fa9bea68b459feb5a42fe191c4834a862bf430f2
homma <dskhomma@gmail.com>
2025-08-10 18:19:16 +0900

    add a test case and clean up

--- scripts/sound_test.gd
+++ scripts/sound_test.gd
@@ -4,37 +4,75 @@ extends Node
 
 var player: MusicPlayer
 
+const c4 = 261.6256
+const d4 = 293.6648
+const e4 = 329.6276
+const f4 = 349.2282
+const g4 = 391.9954
+const a5 = 440.0000
+const b5 = 493.8833
+const c5 = 523.2511
+
 func _ready():
     player = $MusicPlayer
     set_music()
     player.play_music()
 
 func set_music():
-    var music = Music.new()
+    var m = Music.new()
 
-    const c4 = 261.6256
-    const d4 = 293.6648
-    const e4 = 329.6276
-    const f4 = 349.2282
-    const g4 = 391.9954
-    const a5 = 440.0000
-    const b5 = 493.8833
-    const c5 = 523.2511
+    var t: float = 0.0
+    t += add_chord_sample(m, t)
+    t += add_scale_sample(m, t)
+    t += add_melody_sample(m, t)
 
-    music.add_note(0.0, c4, 1)
-    music.add_note(2.0, e4, 1)
-    music.add_note(4.0, g4, 1)
-    music.add_note(6.0, c4, 4)
-    music.add_note(6.0, e4, 4)
-    music.add_note(6.0, g4, 4)
+    player.music = m
 
-    music.add_note(11.0, c4, 1)
-    music.add_note(12.0, d4, 1)
-    music.add_note(13.0, e4, 1)
-    music.add_note(14.0, f4, 1)
-    music.add_note(15.0, g4, 1)
-    music.add_note(16.0, a5, 1)
-    music.add_note(17.0, b5, 1)
-    music.add_note(18.0, c5, 4)
+## t: base time
+func add_chord_sample(m: Music, t: float) -> float:
+    m.a(t + 0.0, c4, 1)
+    m.a(t + 2.0, e4, 1)
+    m.a(t + 4.0, g4, 1)
+    m.a(t + 6.0, c4, 4)
+    m.a(t + 6.0, e4, 4)
+    m.a(t + 6.0, g4, 4)
 
-    player.music = music
+    # elapsed time
+    return 11
+
+## t: base time
+func add_scale_sample(m: Music, t: float) -> float:
+     m.a(t + 0.0, c4, 1)
+     m.a(t + 1.0, d4, 1)
+     m.a(t + 2.0, e4, 1)
+     m.a(t + 3.0, f4, 1)
+     m.a(t + 4.0, g4, 1)
+     m.a(t + 5.0, a5, 1)
+     m.a(t + 6.0, b5, 1)
+     m.a(t + 7.0, c5, 4)
+
+    # elapsed time
+     return 12
+
+## t: base time
+func add_melody_sample(m: Music, t: float) -> float:
+     m.a(t + 0.0, c4, 1)
+     m.a(t + 1.0, c4, 1)
+     m.a(t + 2.0, g4, 1)
+     m.a(t + 3.0, g4, 1)
+     m.a(t + 4.0, a5, 1)
+     m.a(t + 5.0, a5, 1)
+     m.a(t + 6.0, g4, 2)
+
+     t += 8
+
+     m.a(t + 0.0, f4, 1)
+     m.a(t + 1.0, f4, 1)
+     m.a(t + 2.0, e4, 1)
+     m.a(t + 3.0, e4, 1)
+     m.a(t + 4.0, d4, 1)
+     m.a(t + 5.0, d4, 1)
+     m.a(t + 6.0, c4, 2)
+
+    # elapsed time
+     return 19
